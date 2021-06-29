9958422299fc7e28a9da6ddf19c4253b6c9232b9
EugeniusK <eugenekang03@gmail.com>
2021-06-30 08:34:50 +0900

    29th June 2021

--- 2021/06/Q03c.py
+++ 2021/06/Q03c.py
@@ -1,14 +1,25 @@
-count = 0
-theLine = ""
+#Do not use any oher data structure such as an array or list.
+count = 0 #A counter for the line numbers
+theLine = "" #Holds each line of the file
 
+#Open "Cities.txt" as input
 cities_file = open('Cities.txt', 'r')
+#Open "Numbered.txt" as output
 numbered_file = open('Numbered.txt', 'w')
 
+#Use a for/each lioop to read each line of
+#the input file into a variable named 'theline'
 for city in cities_file.readlines():
+    #increment the line count
     count = count + 1
-    city_cleaned = city.replace('\n', '')
-    theLine = f'{count} {city_cleaned}\n'
+    #Add the line umber to the front of the line followed by a space
+    theLine = f'{count} {city}'
+    #print the line to the display
     print(theLine)
+    #write the new line to the output file
     numbered_file.write(theLine)
+
+#close the input file
 cities_file.close()
+#close the output file
 numbered_file.close()
--- 2021/06/Q06.py
+++ 2021/06/Q06.py
@@ -1,7 +1,14 @@
 theArtists = [
     ['Andy', 'Warhol', 1928],
     ['Pablo', 'Picasso', 1881],
-    ['Salvador', 'Dali', 1904]
+    ['Salvador', 'Dali', 1904],
+    ['Lavinia', 'Fontana', 1552],
+    ['Jackson', 'Pollock', 1912],
+    ['Henri', 'Matisse', 1869],
+    ['Frida', 'Kahlo', 1907],
+    ['Georgeia', "O'Keeffe", 1887],
+    ['Kara', 'Walker', 1969],
+    ['Yayoi', 'Kusama', 1929]
 ]
 
 theLabels = []
@@ -15,4 +22,12 @@ for artist in theArtists:
 
 print(theLabels)
 
-print(sorted(theArtists, key=lambda x:x[2])[0])
+youngest_artist_age = 0
+younget_artist = []
+for artist in theArtists:
+    artist_year = artist[2]
+    if artist_year > youngest_artist_age:
+        youngest_artist_age = artist[2]
+        youngest_artist = artist
+
+print(youngest_artist)
